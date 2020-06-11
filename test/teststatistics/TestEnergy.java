e1253c9bc0875d4c1a7944fa26e2c5739cb271f1
David Cook <davidthomascook@aol.com>
2020-06-11 18:27:08 +0100

    Add test file for the Energy class
    Add the test file for the energy class that will have the tests for the
    energy class written in it.

---
+++ test/teststatistics/TestEnergy.java
@@ -0,0 +1,23 @@
+package teststatistics;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Class for creating / testing the Energy class.
+ * @author David Cook
+ * @version 0.1
+ */
+public class TestEnergy {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+}
