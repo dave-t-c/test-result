29612a2f4e41a095ed8b9cd9119f1defbb50cd81
David Cook <davidthomascook@aol.com>
2020-06-11 18:37:49 +0100

    Add test to try and create a new instance of the Energy Class.

--- test/teststatistics/TestEnergy.java
+++ test/teststatistics/TestEnergy.java
@@ -12,12 +12,24 @@ import org.junit.Test;
  */
 public class TestEnergy {
 
+  private double standardCarbs;
+  private double standardCalories;
+
+  /**
+   * Setup all the required values before each test is run.
+   * @throws Exception thrown if an error occurs when setting the values.
+   */
   @Before
   public void setUp() throws Exception {
+    standardCarbs = 25.0;
+    standardCalories = 300.5;
   }
 
+  /**
+   * Test to try and create a new instance of the Energy Object.
+   */
   @Test
-  public void test() {
-    fail("Not yet implemented");
+  public void testCreateEnergy() {
+    Energy testEnergy = new Energy(standardCarbs, standardCalories);
   }
 }
