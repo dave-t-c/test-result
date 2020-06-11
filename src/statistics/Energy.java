802aa7127953300360a60806e58350d2bdb9a9ac
David Cook <davidthomascook@aol.com>
2020-06-11 19:43:04 +0100

    Create the energy class
    Create the energy class that has a constructor that takes 2 doubles and
    also has a getCarbs function that returns 25.0.

---
+++ src/statistics/Energy.java
@@ -0,0 +1,30 @@
+package statistics;
+
+/**
+ * Class for storing the Energy section of a test result.
+ * This class will store the carbohydrate and calorie values 
+ * for each test result.
+ * @author David Cook
+ *
+ */
+public class Energy {
+
+  /**
+   * Constructor for the energy class.
+   * This takes the required carbohydrate and calorie values.
+   * @param carbs - Carbohydrate value for the new entry
+   * @param calories - Calorie value for the new entry
+   */
+  public Energy(double carbs, double calories) {
+    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * Method for retuing the carb value of the Energy Object.
+   * @return double - carb value.
+   */
+  public double getCarbs() {
+    return 25.0;
+  }
+
+}
