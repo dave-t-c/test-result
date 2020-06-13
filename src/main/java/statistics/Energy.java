155c7aac3185536e1c5ae756168c3a7452ba2e47
David Cook <davidthomascook@aol.com>
2020-06-13 21:54:47 +0100

    Add correct hash code method to Energy class.
    Add an improved and correct hash code method to the Energy class. This uses Objects.hash to do this.

--- src/main/java/statistics/Energy.java
+++ src/main/java/statistics/Energy.java
@@ -1,5 +1,7 @@
 package main.java.statistics;
 
+import java.util.Objects;
+
 /**
  * Class for storing the Energy section of a test result.
  * This class will store the carbohydrate and calorie values 
@@ -38,7 +40,7 @@ public class Energy {
   public double getCalories() {
     return this.calories;
   }
-  
+
   /**
    * Checks the equality of an Object with the Energy.
    * Should return true if the contents of the object are equal.
@@ -49,7 +51,10 @@ public class Energy {
     if (this == obj) {
       return true;
     }
-    if (!(obj instanceof Energy)) {
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
       return false;
     }
     Energy other = (Energy) obj;
@@ -59,11 +64,12 @@ public class Energy {
   
   /**
    * Generates a hash code for the Energy Object.
-   * If two values have the same content then this should return the same for both.
+   * If two objects have the same content then this should return the same for both.
+   * @return int - the hash code value for the Energy Object.
    */
   @Override
   public int hashCode() {
-    return 5;
+    return Objects.hash(calories, carbs);
   }
   
 
