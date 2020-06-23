14503a3e9018d128210123adfade31b35c1c8f01
David Cook <davidthomascook@aol.com>
2020-06-23 15:01:35 +0100

    Modify getSubcategoryNames method to use the hash map instead of a different set.
    This means that the sub category names will now be stored in the hash map, as they can be returned by getting the set of keys from the hash map.

--- src/main/java/statistics/CarbNutrition.java
+++ src/main/java/statistics/CarbNutrition.java
@@ -35,7 +35,7 @@ public class CarbNutrition implements NutritionType {
 
   @Override
   public Set<String> getSubcategoryNames() {
-    return nameSet;
+    return categoryMap.keySet();
   }
   
   @Override
