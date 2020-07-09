78713e4a0c07fa9c4417f823e6bde6fe2ec9bddf
David Cook <davidthomascook@aol.com>
2020-07-09 18:00:50 +0100

    Modify the nutrition helper to have a add value method.
    This method simply returns 25 as that is the expected new value.

--- src/main/java/statistics/NutritionHelper.java
+++ src/main/java/statistics/NutritionHelper.java
@@ -1,5 +1,17 @@
 package main.java.statistics;
 
+import java.util.HashMap;
+
+/**
+ * Class for the helper methods for all classes
+ * that implement the nutrition interface.
+ * This class acts as a center location for all of those methods.
+ * @author dave-t-c
+ */
 public class NutritionHelper {
 
+  public double addValue(String category, HashMap<String, Double> nutritionValues, Double value) {
+    return 25.0;
+  }
+  
 }
