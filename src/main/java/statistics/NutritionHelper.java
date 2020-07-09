3fea31b129d5776e879cd854bae21a1d783238a1
David Cook <davidthomascook@aol.com>
2020-07-09 18:04:56 +0100

    Modify addValue method in NutritionHelper to return the value passed to the function.

--- src/main/java/statistics/NutritionHelper.java
+++ src/main/java/statistics/NutritionHelper.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 public class NutritionHelper {
 
   public double addValue(String category, HashMap<String, Double> nutritionValues, Double value) {
-    return 25.0;
+    return value;
   }
   
 }
