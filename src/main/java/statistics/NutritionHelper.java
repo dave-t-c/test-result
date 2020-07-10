a976945a1f8f05a6dc3a35515fed518fe3d1c670
David Cook <davidthomascook@aol.com>
2020-07-10 15:37:27 +0100

    Improve javadoc and add null check to nutrition helper.
    Add the java doc to the add value method declaration, and also add a
    null check, as the non null checks only work in eclipse as a warning,
    there still needs to be value checks.

--- src/main/java/statistics/NutritionHelper.java
+++ src/main/java/statistics/NutritionHelper.java
@@ -11,8 +11,18 @@ import org.eclipse.jdt.annotation.NonNull;
  */
 public class NutritionHelper {
 
+  /**
+   * Method for adding a value to the values HashMap from the NutritionType classes.
+   * @param category - Category to be added.
+   * @param nutritionValues - Map containing the values.
+   * @param value - The value for the category to be added.
+   * @return
+   */
   public double addValue(@NonNull String category, HashMap<String, Double> nutritionValues, 
       @NonNull Double value) {
+    if (category == null || value == null) {
+      return 0.0;
+    }
     nutritionValues.put(category, value);
     return value;
   }
