ec9044689a9281a91e2fdd87b15fbaceaf57e5de
David Cook <davidthomascook@aol.com>
2020-07-22 09:32:34 +0100

    Change value returned in NutritionHelper removeCategory
    A ternary statement is now used, if the entry is null, 0.0 is returned, as the total should be changed by 0.0. If not, then the value is returned.

--- src/main/java/statistics/NutritionHelper.java
+++ src/main/java/statistics/NutritionHelper.java
@@ -36,7 +36,7 @@ public class NutritionHelper {
   
   public Double removeSubcategory(@NonNull String category, 
       @NonNull HashMap<String, Double> nutritionValues) {
-    return nutritionValues.get(category);
+    return nutritionValues.get(category) == null ? 0.0 : nutritionValues.get(category);
   }
   
 
