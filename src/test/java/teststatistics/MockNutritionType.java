b63f5a1c56eacb0e96a3c6838236e5dfc11f4597
David Cook <davidthomascook@live.co.uk>
2020-07-28 20:57:14 +0100

    Modify mock nutrition class to implement the required methods for testing the nutrition helper.

--- src/test/java/teststatistics/MockNutritionType.java
+++ src/test/java/teststatistics/MockNutritionType.java
@@ -1,11 +1,15 @@
 package test.java.teststatistics;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Set;
 import main.java.statistics.NutritionType;
 
+/**
+ * Mock nutrition type for testing NutritionHelper.
+ * @author dave-t-c
+ */
 public class MockNutritionType implements NutritionType {
+  HashMap<String, Double> valuesMap = new HashMap<>();
 
   @Override
   public Double getTotal() {
@@ -19,11 +23,12 @@ public class MockNutritionType implements NutritionType {
 
   @Override
   public void addValue(String name, Double value) {
+    valuesMap.put(name, value);
   }
 
   @Override
   public Set<String> getSubcategoryNames() {
-    return Collections.emptySet();
+    return valuesMap.keySet();
   }
 
   @Override
@@ -38,7 +43,7 @@ public class MockNutritionType implements NutritionType {
 
   @Override
   public HashMap<String, Double> getValues() {
-    return new HashMap<String, Double>();
+    return valuesMap;
   }
 
 }
