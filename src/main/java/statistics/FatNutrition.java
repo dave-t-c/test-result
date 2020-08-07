4ff55cdff7e15552b2a5073b5a028e64caf42631
David Cook <davidthomascook@aol.com>
2020-08-07 20:47:51 +0100

    Create new FatNutrition class
    Implement the NutritionType interface with the getTotal method returning
    0.

---
+++ src/main/java/statistics/FatNutrition.java
@@ -0,0 +1,50 @@
+package main.java.statistics;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class FatNutrition implements NutritionType {
+
+  @Override
+  public Double getTotal() {
+    // TODO Auto-generated method stub
+    return 0.0;
+  }
+
+  @Override
+  public String getName() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void addValue(String name, Double value) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public Set<String> getSubcategoryNames() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Double getSubcategoryValue(String name) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void removeSubcategory(String name) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public HashMap<String, Double> getValues() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
