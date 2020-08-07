f564b4c709d135db4be9c3b976591d068d654f18
David Cook <davidthomascook@aol.com>
2020-08-07 22:42:45 +0100

    Improve javadoc in FatNutrition.

--- src/main/java/statistics/FatNutrition.java
+++ src/main/java/statistics/FatNutrition.java
@@ -3,27 +3,48 @@ package main.java.statistics;
 import java.util.HashMap;
 import java.util.Set;
 
+/**
+ * Class for storing fat related nutrition information.
+ * @author dave-t-c
+ */
 public class FatNutrition implements NutritionType {
 
   Double total = 0.0;
   NutritionHelper helper = new NutritionHelper();
   HashMap<String, Double> values = new HashMap<>();
   
+  /**
+   * Gets the total for the fat in the FatNutrition Object.
+   * @return - Total for the FatNutrition Object.
+   */
   @Override
   public Double getTotal() {
     return total;
   }
 
+  /**
+   * Gets the Name of the NutritionType.
+   * @return Name of NutritionType
+   */
   @Override
   public String getName() {
     return "Fat";
   }
 
+  /**
+   * Adds a value to the values map.
+   * The total for the NutritionType is increased
+   * if it can be added successfully. 
+   */
   @Override
   public void addValue(String name, Double value) {
     total += helper.addValue(name, values, value);
   }
 
+  /**
+   * Get the names of the sub-categories in the values map.
+   * @return Set of Sub-category Names
+   */
   @Override
   public Set<String> getSubcategoryNames() {
     return helper.getSubcategoryNames(values);
