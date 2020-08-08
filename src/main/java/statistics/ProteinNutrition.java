c148a4e99f8ba9d4a1f2c85fe303f9ce249f1b9a
David Cook <davidthomascook@aol.com>
2020-08-08 17:44:29 +0100

    Modify getValues method to return new hash map.
    The hash map returned should be empty as no items have been added.

--- src/main/java/statistics/ProteinNutrition.java
+++ src/main/java/statistics/ProteinNutrition.java
@@ -71,8 +71,7 @@ public class ProteinNutrition implements NutritionType {
 
   @Override
   public HashMap<String, Double> getValues() {
-    // TODO Auto-generated method stub
-    return null;
+    return new HashMap<String, Double>();
   }
 
 }
