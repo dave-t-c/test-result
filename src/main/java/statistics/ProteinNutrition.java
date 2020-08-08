3696108923960617ce973d22d2554fb903988839
David Cook <davidthomascook@aol.com>
2020-08-08 17:00:40 +0100

    Modify getSubcategoryValue method to return 0
    This is becasue the category being searched for has not been added.

--- src/main/java/statistics/ProteinNutrition.java
+++ src/main/java/statistics/ProteinNutrition.java
@@ -52,8 +52,7 @@ public class ProteinNutrition implements NutritionType {
 
   @Override
   public Double getSubcategoryValue(String name) {
-    // TODO Auto-generated method stub
-    return null;
+    return 0.0;
   }
 
   @Override
