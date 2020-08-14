ce150e138eabcc4a655084d7b46f6144989f05c8
David Cook <davidthomascook@live.co.uk>
2020-08-14 20:02:27 +0100

    Add calss for CaloriesNutrition
    This class will store nutrition information realted to calories.

---
+++ src/main/java/statistics/CaloriesNutrition.java
@@ -0,0 +1,53 @@
+package main.java.statistics;
+
+import java.util.HashMap;
+import java.util.Set;
+
+/**
+ * Class for storing Calorie related nutrition information.
+ * @author dave-t-c
+ */
+public class CaloriesNutrition implements NutritionType {
+
+  @Override
+  public Double getTotal() {
+    return 0.0;
+  }
+
+  @Override
+  public String getName() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void addValue(String name, Double value) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public Set<String> getSubcategoryNames() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Double getSubcategoryValue(String name) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void removeSubcategory(String name) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public HashMap<String, Double> getValues() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
