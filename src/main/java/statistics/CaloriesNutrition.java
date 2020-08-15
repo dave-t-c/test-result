4f042f559de4a2d863a67e2aaa71191d25651399
David Cook <davidthomascook@live.co.uk>
2020-08-15 21:57:28 +0100

    Modify addValue method to increase total in CalorieNutrition.
    This total variable is now returned in the getTotal method.

--- src/main/java/statistics/CaloriesNutrition.java
+++ src/main/java/statistics/CaloriesNutrition.java
@@ -9,9 +9,11 @@ import java.util.Set;
  */
 public class CaloriesNutrition implements NutritionType {
 
+  private Double total = 0.0;
+  
   @Override
   public Double getTotal() {
-    return 0.0;
+    return total;
   }
 
   @Override
@@ -22,8 +24,7 @@ public class CaloriesNutrition implements NutritionType {
 
   @Override
   public void addValue(String name, Double value) {
-    // TODO Auto-generated method stub
-
+    total += value;
   }
 
   @Override
